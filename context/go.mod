d32f009527b1b1a2991c2d887d26018fe1a92e7f
snowitty <snowitty@163.com>
2020-07-21 03:23:30 +0800

    Conetext

---
+++ context/go.mod
@@ -0,0 +1,7 @@
+module context
+
+go 1.14
+
+require gee v0.0.0
+
+replace gee => ./gee
---
+++ context/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"gee"
+	"net/http"
+)
+
+func main() {
+	r := gee.New()
+	r.GET("/", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+	})
+
+	r.GET("/hello", func(c *gee.Context) {
+		c.String(http.StatusOK, "hello %s, you are at %s\n", c.Query("name"), c.Path)
+	})
+
+	r.POST("/login", func(c *gee.Context) {
+		c.Json(http.StatusOK, gee.H{
+			"username": c.PostForm("username"),
+			"password": c.PostForm("password"),
+		})
+	})
+
+	r.Run(":9999")
+}

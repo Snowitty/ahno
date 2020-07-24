26e07f077e338bff61d337529f8388bad656e5d6
Snowitty <snowitty@163.com>
2020-07-24 13:40:44 +0800

    Painc recovery

--- main.go
+++ main.go
@@ -1,50 +1,20 @@
 package main
 
 import (
-	"fmt"
-	"html/template"
 	"net/http"
-	"time"
 
 	"gee"
 )
 
-type student struct {
-	Name string
-	Age  int8
-}
-
-func formatAsDate(t time.Time) string {
-	year, month, day := t.Date()
-	return fmt.Sprintf("%d-%02d-%02d", year, month, day)
-}
-
 func main() {
-	r := gee.New()
-	r.Use(gee.Logger())
-	r.SetFuncMap(template.FuncMap{
-		"formatAsDate": formatAsDate,
-	})
-	r.LoadHTMLGlob("templates/*")
-	r.Static("/assets", "./static")
-
-	stu1 := &student{Name: "wt", Age: 34}
-	stu2 := &student{Name: "Snowitty", Age: 18}
+	r := gee.Default()
 	r.GET("/", func(c *gee.Context) {
-		c.HTML(http.StatusOK, "css.tmpl", nil)
+		c.String(http.StatusOK, "Hello Gee\n")
 	})
-	r.GET("/students", func(c *gee.Context) {
-		c.HTML(http.StatusOK, "arr.tmpl", gee.H{
-			"title":  "gee",
-			"stuArr": [2]*student{stu1, stu2},
-		})
-	})
-
-	r.GET("/date", func(c *gee.Context) {
-		c.HTML(http.StatusOK, "custom_func.tmpl", gee.H{
-			"title": "gee",
-			"now":   time.Date(2019, 8, 17, 0, 0, 0, 0, time.UTC),
-		})
+	// index out of range for testing Recovery()
+	r.GET("/panic", func(c *gee.Context) {
+		names := []string{"geektutu"}
+		c.String(http.StatusOK, names[100])
 	})
 
 	r.Run(":9999")
